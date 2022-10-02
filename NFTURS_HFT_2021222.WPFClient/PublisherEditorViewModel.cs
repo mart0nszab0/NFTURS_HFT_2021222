4e4af4042cad757d2c0f401b6dbb00adc64602d2
martonszabo <martonszabo@stud.uni-obuda.hu>
2022-10-02 20:48:51 +0200

    PublisherEditor fully implemented with all CRUD methods.

---
+++ NFTURS_HFT_2021222.WPFClient/PublisherEditorViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using NFTURS_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace NFTURS_HFT_2021222.WPFClient
+{
+    public class PublisherEditorViewModel : ObservableRecipient
+    {
+        public RestCollection<Publisher> Publishers { get; set; }
+
+        private Publisher selectedPublisher;
+
+        public Publisher SelectedPublisher
+        {
+            get { return selectedPublisher; }
+            set
+            {
+
+                //if (value == null) return;
+
+                if (value != null)
+                {
+                    selectedPublisher = new Publisher()
+                    {
+                        Name = value.Name,
+                        PublisherId = value.PublisherId
+                    };
+
+                    OnPropertyChanged();
+                    (DeletePublisherCommand as RelayCommand).NotifyCanExecuteChanged();
+
+
+                }
+                //SetProperty(ref selectedPublisher, value);
+            }
+        }
+
+
+        public ICommand CreatePublisherCommand { get; set; }
+        public ICommand UpdatePublisherCommand { get; set; }
+        public ICommand DeletePublisherCommand { get; set; }
+
+        public bool DesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public PublisherEditorViewModel()
+        {
+            if (!DesignMode)
+            {
+                Publishers = new RestCollection<Publisher>("http://localhost:32095/", "Publisher");
+
+                CreatePublisherCommand = new RelayCommand(() =>
+                {
+                    Publishers.Add(new Publisher() { Name = SelectedPublisher.Name });
+                });
+
+                UpdatePublisherCommand = new RelayCommand(() =>
+                {
+                    Publishers.Update(SelectedPublisher);
+                });
+
+                DeletePublisherCommand = new RelayCommand(() =>
+                {
+                    Publishers.Delete(SelectedPublisher.PublisherId);
+                },
+                () =>
+                {
+                    return SelectedPublisher != null;
+                });
+
+                SelectedPublisher = new Publisher();
+            }
+        }
+
+
+    }
+}
