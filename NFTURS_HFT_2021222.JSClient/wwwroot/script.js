74c421e19cf316fb289cf2385497326127a73f76
martonszabo <martonszabo@stud.uni-obuda.hu>
2022-10-03 12:19:20 +0200

    js: "read function" implemented

--- NFTURS_HFT_2021222.JSClient/wwwroot/script.js
+++ NFTURS_HFT_2021222.JSClient/wwwroot/script.js
@@ -1,1 +1,21 @@
-﻿
+﻿let games = [];
+
+fetch('http://localhost:32095/Game')
+    .then(x => x.json())
+    .then(y => {
+        games = y;
+        console.log(games);
+        display();
+        });
+
+function display() {
+    games.forEach(g => {
+        document.getElementById('gamedata')
+            .innerHTML += "<tr><td>" + g.gameId + "</td><td>"
+        + g.name + "</td><td>"
+        + g.publisher.name + "</td><td>"
+        + g.gameRating + "</td></tr>";
+
+        console.log(g.name);
+    });
+}
